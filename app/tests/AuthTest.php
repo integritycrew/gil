f0474b9832ce6aaf556ea45ae9a825d34fcbeada
Maciej ze4lot Płocki <maciek@ze4lot.pl>
2014-07-19 14:52:47 +0200

    Modify tests cases

--- app/tests/AuthTest.php
+++ app/tests/AuthTest.php
@@ -2,15 +2,36 @@
 
 class AuthTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
 	public function testAuthFailure()
 	{
         $response = $this->call('GET', 'api/v1');
-        $this->assertEquals('Protected resource', $response->getContent());
+        $this->assertEquals(403, json_decode($response->getContent())->code);
 	}
+
+    public function testCreateTestUser()
+    {
+        $user = array(
+            'email'    => 'test@email.com',
+            'password' => 'secret123'
+        );
+        $response = $this->call('POST', 'api/v1/user', $user);
+        $this->assertEquals(201, json_decode($response->getContent())->code);
+    }
+
+    public function testCreateTestUserWithUsedEmailAddress()
+    {
+        $user = array(
+            'email'    => 'test@email.com',
+            'password' => 'secret123'
+        );
+        $response = $this->call('POST', 'api/v1/user', $user);
+        $this->assertEquals(403, json_decode($response->getContent())->code);
+    }
+
+    public function testDeleteTestuser()
+    {
+        $response = $this->call('DELETE', 'api/v1/user/'.User::where('email', '=', 'test@email.com')->first()->id);
+        $this->assertEquals(200, json_decode($response->getContent())->code);
+    }
 
 }
