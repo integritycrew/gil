bca44b86ec23a8af8e43ab94e33c19611fe8b459
Maciej ze4lot Płocki <maciek@ze4lot.pl>
2014-07-28 01:37:34 +0200

    Update database config file

--- app/config/database.php
+++ app/config/database.php
@@ -83,16 +83,16 @@ return array(
 	|
 	*/
 
-    //	'redis' => array(
-    //
-    //		'cluster' => false,
-    //
-    //		'default' => array(
-    //			'host'     => '127.0.0.1',
-    //			'port'     => 6379,
-    //			'database' => 0,
-    //		),
-    //
-    //	),
+    'redis' => array(
+
+        //'cluster' => false,
+
+        //'default' => array(
+        //    'host'     => '127.0.0.1',
+        //    'port'     => 6379,
+        //    'database' => 0,
+        //),
+
+    ),
 
 );
